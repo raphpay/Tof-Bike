a79865cb444e13248870560e42f5fb0bfd5c9e40
raphpay <raphpay.dev@gmail.com>
2025-07-25 10:11:42 +0400

    Feat: Integrate basic navigation #3

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,18 @@
-import Footer from "./ui/components/Footer";
-import NavBar from "./ui/components/NavBar";
-import Section from "./ui/components/Section";
-import ContactSection from "./ui/section/ContactSection";
-import HeroSection from "./ui/section/HeroSection";
-import RentalSection from "./ui/section/RentalSection";
-import ReviewsSection from "./ui/section/ReviewsSection";
-import ShopSection from "./ui/section/ShopSection";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import HomePage from "./ui/pages/HomePage";
+
+const AppContent: React.FC = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+    </Routes>
+  );
+};
 
 export default function App() {
-  const sections = [
-    { key: "home", label: "Home" },
-    { key: "shop", label: "Magasin" },
-    { key: "rental", label: "Location" },
-    { key: "reviews", label: "Avis" },
-    { key: "contact", label: "Contact" },
-  ];
-
   return (
-    <div>
-      <NavBar />
-      <div
-        className="h-screen w-full snap-y snap-mandatory overflow-y-scroll scroll-smooth"
-        style={{ WebkitOverflowScrolling: "touch" }}
-      >
-        {sections.map((section) => (
-          <Section key={section.key} id={section.key}>
-            {section.key === "home" ? (
-              <HeroSection />
-            ) : section.key === "shop" ? (
-              <ShopSection />
-            ) : section.key === "rental" ? (
-              <RentalSection />
-            ) : section.key === "reviews" ? (
-              <ReviewsSection />
-            ) : section.key === "contact" ? (
-              <ContactSection />
-            ) : (
-              <div className="flex h-full w-full flex-col items-center justify-center">
-                <h1 className="mb-4 text-4xl font-extrabold text-gray-900 md:text-6xl">
-                  {section.label}
-                </h1>
-                <p className="text-lg text-gray-500">
-                  Section de démonstration: {section.label}
-                </p>
-                <button className="h-25 w-50 text-black"></button>
-              </div>
-            )}
-          </Section>
-        ))}
-      </div>
-      <Footer />
-    </div>
+    <Router>
+      <AppContent />
+    </Router>
   );
 }
