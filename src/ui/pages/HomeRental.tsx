4a7e3e4ff798695fd7257f686e6c4e6ac599a028
raphpay <raphpay.dev@gmail.com>
2025-08-17 09:30:06 +0400

    Integrate shadcn button and theme #29

--- src/ui/pages/HomeRental.tsx
+++ src/ui/pages/HomeRental.tsx
@@ -1,22 +1,31 @@
 import { useNavigate } from "react-router-dom";
-import Button from "../components/Button";
+import GoogleLogo from "../../../public/icons8-google.svg";
+import { Button as NeoButton } from "../../components/ui/button";
 import RentalNavBar from "../components/RentalNavBar";
 
 export default function HomeRental() {
   const navigate = useNavigate();
+
+  function navigateToGoogle() {}
+
   return (
     <div>
       <RentalNavBar />
       <div className="flex min-h-screen w-screen flex-col items-center justify-center gap-2">
-        <Button
-          title="Je m'enregistre pour la location"
-          onClick={() => navigate("formulaire-location")}
-        />
-        <Button
-          title="Laisser un avis"
-          variant="secondary"
-          href="http://bit.ly/3UhVsql"
-        />
+        <NeoButton onClick={() => navigate("formulaire-location")}>
+          Je m'enregistre
+        </NeoButton>
+        <NeoButton variant="neutral" onClick={navigateToGoogle}>
+          <a
+            href="http://bit.ly/3UhVsql"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex flex-row items-center gap-2"
+          >
+            <img src={GoogleLogo} width={20} height={20} />
+            Laisser un avis
+          </a>
+        </NeoButton>
       </div>
     </div>
   );
